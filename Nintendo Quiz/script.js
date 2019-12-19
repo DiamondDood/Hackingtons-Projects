aa906c24eb363f7d473ad7b769a8d6b777664e48
DiamondDood <56020884+DiamondDood@users.noreply.github.com>
2019-12-19 10:25:17 +0800

    Files brought over from Hackingtons

---
+++ Nintendo Quiz/script.js
@@ -0,0 +1,90 @@
+
+var money = 0
+
+
+var q1 = prompt("Who made Nintendo?")
+if(q1 === "Fusajiro Yamauchi"){
+  alert ("Correct")
+  money = money + 10
+  alert("You have $" + money)
+}
+else{
+  alert("Wrong")
+  money = money - 10
+  alert("You have $" + money)
+}
+
+
+
+
+var q2 = prompt("When was the nintendo 64 released?")
+if(q2 === "June 1996 in Japan"){
+  alert ("Correct")
+  money = money + 10
+  alert("You have $" + money)
+}
+else{
+  alert("Wrong")
+  money = money - 10
+  alert("You have $" + money)
+}
+
+
+
+
+var q3 = prompt("When was Nintendo founded?")
+if(q3 === "September 23, 1889"){
+  alert ("Correct")
+  money = money + 10
+  alert("You have $" + money)
+}
+else{
+  alert("Wrong")
+  money = money - 10
+  alert("You have $" + money)
+}
+
+
+
+
+var q4 = prompt("Did Nintendo make Sonic The Hedgehog?")
+if(q4 === "No"){
+  alert ("Correct")
+  money = money + 10
+  alert("You have $" + money)
+}
+else{
+  alert("Wrong")
+  money = money - 10
+  alert("You have $" + money)
+}
+
+
+
+
+var q5 = prompt("Mario was originally called?")
+if(q5 === "Jump Man"){
+  alert ("Correct")
+  money = money + 10
+  alert("You have $" + money)
+}
+else{
+  alert("Wrong")
+  money = money - 10
+  alert("You have $" + money)
+}
+
+
+
+
+var q5 = prompt("Do you want tacos")
+if(q5 === "Yes"){
+  alert ("Correct")
+  money = money + 10
+  alert("You have $" + money)
+}
+else{
+  alert("Wrong")
+  money = money - 10
+  alert("You have $" + money)
+}
---
+++ Will is da best/script.js
@@ -0,0 +1,15 @@
+var password = "will is da best"
+var correct = "will is da best"
+var wrong = "will is da best1"
+
+var answer = prompt("will is da best")
+
+if(password === answer){
+  alert(correct)
+  }
+
+else{
+  alert(wrong); 
+  window.location.href = "bob.html"
+  }
+
